f351cedb7901a4943e3c633bcfc29a3127bb1471
KhrisDV <KhrisQ@protonmail.com>
2022-04-29 12:43:40 +0200

    get, post, put & delete working

--- src/controllers/tasksControllers.mjs
+++ src/controllers/tasksControllers.mjs
@@ -1,14 +1,29 @@
 import { tasks } from "../models/tasksModels.mjs";
 
-export function getHolaMundoController(request, response) {
-  response.send("Hola mundo");
-}
-
 export function getTaskController(request, response) {
   response.json(tasks);
 }
 
 export function postTaskController(request, response) {
-  tasks.push(request.body);
-  response.sendStatus(201);
+  try {
+    tasks.push({ ...request.body, id: Date.now() });
+    response.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    response.sendStatus(500);
+  }
+}
+
+export function putTaskController(request, response) {
+  const updatedTask = request.body;
+  const oldTaskIdx = tasks.findIndex((item) => item.id === updatedTask.id);
+  tasks[oldTaskIdx] = updatedTask;
+  response.sendStatus(200);
+}
+
+export function deleteTaskController(request, response) {
+  const updatedTask = request.body;
+  const oldTaskIdx = tasks.findIndex((item) => item.id === updatedTask.id);
+  tasks.splice(oldTaskIdx, 1);
+  response.sendStatus(200);
 }
--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,24 @@
 import express from "express";
 import {
-  getHolaMundoController,
   getTaskController,
   postTaskController,
+  putTaskController,
+  deleteTaskController,
 } from "./controllers/tasksControllers.mjs";
+import { postUserController } from "./controllers/usersControllers.mjs";
+import { authMiddleware } from "./middlewares/authorization.mjs";
 
 const app = express();
 const HOST = "/api/v0.0";
 const jsonParser = express.json();
 
-app.get(HOST + "/", getHolaMundoController);
+app.post(HOST + "/users/", jsonParser, postUserController);
 
 app.get(HOST + "/tasks/", getTaskController);
-app.post(HOST + "/task/", jsonParser, postTaskController);
+app.post(HOST + "/task/", jsonParser, authMiddleware, postTaskController);
+app.post(HOST + "/task/", jsonParser, authMiddleware, postTaskController);
+app.put(HOST + "/task/", jsonParser, authMiddleware, putTaskController);
+app.delete(HOST + "/task/", jsonParser, authMiddleware, deleteTaskController);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Express running...");
