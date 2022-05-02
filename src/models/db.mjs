12b995c8960851b07128c7b3293b0598aacbb3f4
KhrisDV <KhrisQ@protonmail.com>
2022-05-02 12:55:14 +0200

    add db

---
+++ src/models/db.mjs
@@ -0,0 +1,28 @@
+import sqlite3 from "sqlite3";
+
+export const db = new sqlite3.Database("./tasks.db", (err) => {
+  if (err) {
+    throw err.message;
+  }
+  console.log("Connected to the chat database.");
+});
+
+db.run(`
+    CREATE TABLE
+        IF NOT EXISTS
+        users(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+`);
+
+db.run(`
+    CREATE TABLE
+        IF NOT EXISTS
+        tasks (
+            id INTEGER PRIMARY KEY,
+            description VARCHAR(100) NOT NULL,
+            done BOOLEAN DEFAULT false NOT NULL
+        )
+`);
